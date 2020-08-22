4882ca26ab680ed43c8d790c4a3eca624b762d63
Gabriel Barker <gabrielbarker@gmail.com>
2020-08-23 00:57:45 +0100

    TEST the getter methods on conditional

--- src/test/Conditional.test.ts
+++ src/test/Conditional.test.ts
@@ -3,8 +3,32 @@ import Conditional from "../main/Conditional";
 describe("Conditional", () => {
   describe("newInstance", () => {
     it("should be created with three parameters", () => {
-      const conditional: Conditional = Conditional.newInstance("test path", 0, 0);
+      const conditional: Conditional = Conditional.newInstance("path", 0, 0);
       expect(conditional).toBeTruthy();
+    });
+  });
+
+  describe("getFilePath", () => {
+    it("should return correct file path", () => {
+      const filePath: string = "test path";
+      const conditional: Conditional = Conditional.newInstance(filePath, 0, 0);
+      expect(conditional.getFilePath()).toEqual(filePath);
+    });
+  });
+
+  describe("getLineNumber", () => {
+    it("should return correct line number", () => {
+      const lineNumber: number = 3;
+      const conditional: Conditional = Conditional.newInstance("test path", lineNumber, 0);
+      expect(conditional.getLineNumber()).toEqual(lineNumber);
+    });
+  });
+
+  describe("getColumnNumber", () => {
+    it("should return correct column number", () => {
+      const columnNumber: number = 3;
+      const conditional: Conditional = Conditional.newInstance("test path", 0, columnNumber);
+      expect(conditional.getColumnNumber()).toEqual(columnNumber);
     });
   });
 });
