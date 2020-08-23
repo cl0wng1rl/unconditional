8ff9279cd5379ed6cade10baf0ff00f75ffc6329
Gabriel Barker <gabrielbarker@gmail.com>
2020-08-23 17:44:08 +0100

    PRINT the positions of all conditionals in ./main

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,6 @@ import ConditionalDetector from "./main/ConditionalDetector";
 
 async function run(): Promise<void> {
   try {
-    // `who-to-greet` input defined in action metadata file
-    // const nameToGreet = core.getInput("who-to-greet");
-    // console.log(`Hello ${nameToGreet}!`);
-    // const time = new Date().toTimeString();
-    // core.setOutput("time", time);
-    // // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2);
-    // console.log(`The event payload: ${payload}`);
-    console.log("./");
-    console.log(readdirSync("./"));
-    console.log(__dirname);
-    console.log(readdirSync(__dirname));
-    console.log("../");
-    console.log(readdirSync("../"));
-    console.log("./src/");
-    console.log(readdirSync("./src/"));
-    console.log("./src/main/");
-    console.log(readdirSync("./src/main/"));
-
     const mainPath = "./src/main";
     const files = readdirSync(mainPath);
     files.forEach((file) => {
@@ -31,7 +12,7 @@ async function run(): Promise<void> {
       const cond = new ConditionalDetector(path);
       const positionList = cond
         .getConditionals()
-        .map((c) => `\nln:${c.getLineNumber()}, col:${c.getColumnNumber()}`)
+        .map((c) => `\n - ln:${c.getLineNumber()}, col:${c.getColumnNumber()}`)
         .join("");
       console.log(`${file}:${positionList}`);
     });
