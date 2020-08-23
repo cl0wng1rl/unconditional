34c3c823151b4c37cb220d744c150db176bdb114
Gabriel Barker <gabrielbarker@gmail.com>
2020-08-23 18:05:50 +0100

    REMOVE a console log

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
-import { readdirSync } from "fs";
 import ConditionalDetector from "./main/ConditionalDetector";
 import PathRetriever from "./main/PathRetriever";
 
 async function run(): Promise<void> {
   try {
     const files = await new PathRetriever().getPaths();
-    console.log(files);
     files.forEach((file) => {
       const cond = new ConditionalDetector(file);
       const positionList = cond
