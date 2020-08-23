22daea7145629d08982fa6b4c57f57d236d456a8
Gabriel Barker <gabrielbarker@gmail.com>
2020-08-23 17:08:29 +0100

    CHANGE the path

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ async function run(): Promise<void> {
     // // Get the JSON webhook payload for the event that triggered the workflow
     // const payload = JSON.stringify(github.context.payload, undefined, 2);
     // console.log(`The event payload: ${payload}`);
-    const mainPath = "src/main";
-    const files = readdirSync("src/main");
+    const mainPath = "../src/main";
+    const files = readdirSync(mainPath);
     files.forEach((file) => {
       const path = `${__dirname}/${mainPath}/${file}`;
       const cond = new ConditionalDetector(path);
