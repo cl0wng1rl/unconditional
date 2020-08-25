c4f5f02a228adb15c9b2d6fa7302570408a551b6
Gabriel Barker <gabrielbarker@gmail.com>
2020-08-25 01:02:47 +0100

    REMOVE configuration and use action inputs

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,30 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 import ConditionalDetector from "./main/ConditionalDetector";
 import FileRetriever from "./main/FileRetriever";
-import Configuration from "./main/Configuration";
 
 async function run(): Promise<void> {
   try {
-    const config = new Configuration();
-    const files = await new FileRetriever().getPaths(config.getInclude(), config.getExclude());
-    files.forEach((file) => {
-      const cond = new ConditionalDetector(file);
-      const positionList = cond
-        .getConditionals()
-        .map((c) => `\n - ln:${c.getLineNumber()}, col:${c.getColumnNumber()}`)
-        .join("");
-      console.log(`${file}:${positionList}`);
-    });
+    const include = core.getInput("include");
+    const exclude = core.getInput("exclude");
+    const conditionalLayer = core.getInput("conditionalLayer");
+    const max = core.getInput("max");
+    console.log("include");
+    console.log(include);
+    console.log("exclude");
+    console.log(exclude);
+    console.log("conditionalLayer");
+    console.log(conditionalLayer);
+    console.log("max");
+    console.log(max);
+    // const files = await new FileRetriever().getPaths(include, exclude);
+    // files.forEach((file) => {
+    //   const cond = new ConditionalDetector(file);
+    //   const positionList = cond
+    //     .getConditionals()
+    //     .map((c) => `\n - ln:${c.getLineNumber()}, col:${c.getColumnNumber()}`)
+    //     .join("");
+    //   console.log(`${file}:${positionList}`);
+    // });
   } catch (error) {
     core.setFailed(error.message);
   }
